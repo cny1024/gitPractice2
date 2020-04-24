4640556664258f453ba913db96163de8345d9025
cny1024 <64158429+cny1024@users.noreply.github.com>
2020-04-24 15:27:54 -0500

    Update AllGood.java

--- src/day_1/AllGood.java
+++ src/day_1/AllGood.java
@@ -3,5 +3,6 @@ package day_1;
 public class AllGood {
     public static void main(String[] args) {
         System.out.println("yauvvvv yildim yildimmmmmm");
+        System.out.println("yauvvvv yildim yildimmmmmm2222222");
     }
 }
